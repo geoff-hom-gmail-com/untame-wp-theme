91dbec3b628669c3fa3c4ed9dac51f5ce38a9426
Geoffrey Hom <geoffhom@gmail.com>
2014-06-16 05:35:02 -0700

    Working on header. --menu font working (partially). May be regular version, not xlight. --menu centered. --may need to adjust spacing.

--- header.php
+++ header.php
@@ -31,13 +31,12 @@
 		<?php endif; // End header image check. ?>
 
 		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</div>
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle"><?php _e( 'Primary Menu', 'untame' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'after' => '<li class="menu-divider">|</li>' ) ); ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
