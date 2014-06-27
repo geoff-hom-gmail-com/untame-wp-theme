c2cfbbed854a6051e80cab7ee6fdb6c1e521f61a
Geoffrey Hom <geoffhom@gmail.com>
2014-06-27 04:50:32 -0700

    Subpages of "Games" will no longer show social box. (Not that robust, as it uses the page ID, not the page name.)

--- untame-page.php
+++ untame-page.php
@@ -4,11 +4,14 @@
  */
 get_header(); ?>
 <div id="primary" class="content-area">
-<main id="main" class="site-main" role="main">	
-    	<div class="social">  
+<main id="main" class="site-main" role="main">
+	<!-- 2 = page ID for Games -->
+	<?php if ( '2' != $post->post_parent ) {?>
+		<div class="social">  
     		<div class="fb-like" data-href="https://www.facebook.com/untamegames" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></div>
     		<a href="https://twitter.com/untamegames" class="twitter-follow-button" data-show-count="false">Follow @untamegames</a>
-	</div><!-- .social -->
+		</div><!-- .social -->
+	<?php } ?>
 	<?php while ( have_posts() ) : the_post(); ?>
 		<?php get_template_part( 'content', 'page' ); ?>
 	<?php endwhile; // end of the loop. ?>
